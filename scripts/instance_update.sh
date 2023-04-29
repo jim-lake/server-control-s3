a9b0a3a9fcc2fddf3316d5ba9398e2b246d4adc9
jlake <jim@blueskylabs.com>
2023-04-29 10:29:50 -0700

    better logging on startup script

--- scripts/instance_update.sh
+++ scripts/instance_update.sh
@@ -4,7 +4,10 @@ source <( wget "http://169.254.169.254/latest/user-data" -O - 2>/dev/null )
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ "${SC_UPDATE_URL}" != "" ]; then
+  echo `date` ": Updating using URL: $SC_UPDATE_URL" >>/tmp/instance-update.log
   pushd $SC_REPO_DIR >/dev/null 2>/dev/null
   su node -c "${SCRIPT_DIR}/update_to_hash.sh ${SC_UPDATE_URL}" >>/tmp/instance-update.log 2>&1
   popd >/dev/null 2>/dev/null
+else
+  echo `date` ": No SC_UPDATE_URL so not doing anything" >>/tmp/instance-update.log
 fi
