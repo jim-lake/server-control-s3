24d29e751f618c972972c40a42fb98222d1e1c3a
Jim Lake <jim@blueskylabs.com>
2025-07-11 14:35:48 +0000

    ts example

---
+++ example/example2.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import serverControl from '../dist/server_control.js';
+
+import * as config from './config.json';
+
+const port = 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/dump', (req, res) => {
+  res.send({ header: req.headers, query: req.query, body: req.body });
+});
+
+let metadata_opts;
+if (process.platform === 'darwin') {
+  metadata_opts = {
+    httpOptions: { connectTimeout: 5, timeout: 5 },
+    maxRetries: 0,
+  };
+}
+
+const opts = {
+  secret: config.SECRET || 'secure',
+  port: 80,
+  region: config.AWS_REGION,
+  asg_name: config.ASG_NAME,
+  remote_repo_prefix: config.S3_REPO_PREFIX,
+  metadata_opts,
+  repo_dir: config.TEST_REPO_DIR,
+};
+serverControl.init(app, opts);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
